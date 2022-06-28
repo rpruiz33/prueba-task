794d0a350d8e5204e438f81e101c5b48977e097d
rpruiz33 <70825109+rpruiz33@users.noreply.github.com>
2022-06-28 16:08:47 -0300

    probando

---
+++ src/app/mock-Tasks.ts
@@ -0,0 +1,34 @@
+import {Task }  from "./Task"
+export const TASKS :Task[] =  [
+    {
+    id: 1,
+    text:"terminar el modulo de angular",
+    day : "agosto 5 a las 12 hs",
+    reminder : true
+
+
+ }  ,
+ {
+    id: 2,
+    text:"hacer las compras",
+    day : "agosto 5 a las 17 hs",
+    reminder : true
+
+
+ } ,{
+    id: 3,
+    text:"inveestigar sobre angular",
+    day : "agosto 5 a las 18 hs",
+    reminder : false
+
+
+ } ,
+ {
+    id: 4,
+    text:"leer mi libro favorito",
+    day : "agosto 5 a las 20 hs",
+    reminder : false
+
+
+ } 
+]
